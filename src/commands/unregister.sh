ecb6abf78ca387a560c151a3662019651b642467
drcygnus <drcygnus@gmail.com>
2010-11-16 23:11:11 -0800

    unregister: TODO comments
    Ignore-this: e565edbbb0a52c40f7507d3946f91f32
    
    darcs-hash:20101117071111-e018d-aad0c9b1dfc57384fe9426b0b1b06c7d342ca736.gz

--- src/commands/unregister.sh
+++ src/commands/unregister.sh
@@ -3,6 +3,15 @@ function prj-unregister {
     local path=$(pwd)
     local project_name=$(basename $path)
 
+    # XXX make it so that a path can be unregistered even if it is not
+    # a project (i.e., it got removed but is still registered)
+
+    # Also do something about registered projects that don't even
+    # exist
+
+    # Also make it so you can specify a project name OR path instead
+    # of assuming the cwd
+
     (is_project $path ||
         (echo "$path is not a project; run prj-init instead" && return 1)) && \
         (ensure_registered_path $path ||
